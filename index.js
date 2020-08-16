ac25ad89d3a43d0f2b4b8477654b32db91eedb01
Daniel Fuentes <fuentes.danav@gmail.com>
2020-08-16 14:31:21 -0500

    Completed basic functionality

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
 const express = require("express")
 const app = express()
-const fs = require("fs")
 const Datastore = require("nedb")
-const tmpPath = "tmp/locations.json"
+const { response } = require("express")
 
-let requests = null
 const database = new Datastore("database/database.db")
 database.loadDatabase()
-
-const writeLocations = locationStr => {
-  fs.writeFile(tmpPath, locationStr, err => {
-    if (err) throw err
-  })
-}
-
-const readLocations = (() => {
-  fs.readFile(tmpPath, "utf8", function (err, data) {
-    if (err) writeLocations
-    requests = data === undefined ? [] : JSON.parse(data)
-  })
-})()
 
 app.listen(3000, () => console.log("listening at 3000"))
 app.use(express.static("public"))
 app.use(express.json({ limit: "1mb" }))
 
+app.get("/api", (req, res) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      res.end()
+      return
+    }
+    res.json(data)
+  })
+})
+
 app.post("/api", (req, res) => {
-  const { lat, lon } = req.body
-  requests.push({ lat, lon })
-  writeLocations(JSON.stringify(requests))
+  const data = req.body
+  const timestamp = Date.now()
+  data.timestamp = timestamp
+  database.insert(data)
   res.json({
-    status: "success",
-    latitude: lat,
-    longitude: lon,
-    reqPerformed: requests,
+    data,
   })
 })
